99e1cacafe5076e93a13eefb96281d3cf06da398
Marcus <imkay2712@gmail.com>
2022-11-03 15:34:11 +0700

    build menu-footer

--- footer.php
+++ footer.php
@@ -1,4 +1,14 @@
-
+<section class="footer">
+    <div class="mc-container">
+        <div class="menu-footer">
+            <?php
+                wp_nav_menu( array(
+                    'theme_location' => 'menu_footer',
+                ) );
+            ?>
+        </div>
+    </div>
+</section>
 <?php wp_footer() ?>
 </body>
 
