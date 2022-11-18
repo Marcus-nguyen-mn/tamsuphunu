a290d1652cb90ba9001c6127db862a1b738633db
Marcus <imkay2712@gmail.com>
2022-11-18 15:31:18 +0700

    create template contact

---
+++ page-templates/contact-template.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * template name: Contact Template
+ */
+get_header();
+?>
+<?php
+    get_template_part('sections/home/focus'); 
+    get_template_part('sections/banner-top-on-all-page');
+?>
+
+<?php get_footer();?>
