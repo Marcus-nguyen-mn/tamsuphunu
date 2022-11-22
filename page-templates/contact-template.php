1ebbabc03cae6da4338301746c69ca1133c8bbb7
Marcus <imkay2712@gmail.com>
2022-11-22 09:59:59 +0700

    build contact page

--- page-templates/contact-template.php
+++ page-templates/contact-template.php
@@ -7,6 +7,7 @@ get_header();
 <?php
     get_template_part('sections/home/focus'); 
     get_template_part('sections/banner-top-on-all-page');
+    get_template_part('sections/contact/contact-1');
 ?>
 
 <?php get_footer();?>
---
+++ sections/contact/contact-1.php
@@ -0,0 +1,40 @@
+<section class="contact_one">
+    <div class="mc-container">
+        <div class="titl-contact-sec-1">
+            <?php echo "Liên hệ quảng cáo" ?>
+        </div>
+        <div class="form-contact-sec-1">
+            <form action="" id=""  name="" class="form-tag-sec-1">
+                <div class="mc-row mc-mg--15">
+                    <div class="mc-ctf-sec-1 mc-col-6 mc-pdx-15">
+                        <label for="hoTen">Họ và tên:</label>
+                        <div class="mc-inpt-st">
+                            <input type="text" name="hoTen" id="hoTen" />
+                        </div>
+                    </div>
+                    <div class="mc-ctf-sec-1 mc-col-6 mc-pdx-15">
+                        <label for="sdtMc">Số điện thoại:</label>
+                        <div class="mc-inpt-st">
+                            <input type="number" name="sdtMc" id="sdtMc" />
+                        </div>
+                    </div>
+                    <div class="mc-ctf-sec-1 mc-col-12 mc-pdx-15">
+                        <label for="emailMc">Email:</label>
+                        <div class="mc-inpt-st">
+                            <input type="text" name="emailMc" id="emailMc" />
+                        </div>
+                    </div>
+                    <div class="mc-ctf-sec-1 mc-col-12 mc-pdx-15">
+                        <label for="contentMc">Nội dung:</label>
+                        <div class="mc-inpt-st">
+                            <textarea name="contentMc" id="contentMc"></textarea>
+                        </div>
+                    </div>
+                </div>
+                <div class="inp-sub-mc">
+                    <input type="submit" class="btn-inp-sub-mc" value="Gửi"/>
+                </div>
+            </form>
+        </div>
+    </div>
+</section>
