089d997749721ce591525e64fea7ec401647d1b1
Marcus <imkay2712@gmail.com>
2022-11-22 11:15:44 +0700

    fix contact page

--- page-templates/contact-template.php
+++ page-templates/contact-template.php
@@ -7,8 +7,9 @@ get_header();
 <?php
     get_template_part('sections/home/focus'); 
     get_template_part('sections/banner-top-on-all-page');
-    get_template_part('sections/contact/contact-1');
     get_template_part('sections/contact/contact-3');
+    get_template_part('sections/contact/contact-1');
+    
     get_template_part('sections/contact/contact-2');
 ?>
 
