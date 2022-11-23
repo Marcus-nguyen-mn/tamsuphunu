769d20cce38bfa29cf99ef300537c6e3372669c7
Marcus <imkay2712@gmail.com>
2022-11-23 10:01:12 +0700

    build single page

--- functions.php
+++ functions.php
@@ -31,23 +31,3 @@ unset($file, $filePath);
 //         exit;
 //     }
 // }
-function loop_single_related_post($sl) {
-    global $post;
-    $categories = get_the_category($post->ID);
-    if ($categories) {
-        $category_ids = array();
-        foreach($categories as $individual_category) $category_ids[] = $individual_category->term_id;
-        $args=array(
-            'category__in' => $category_ids,
-            'post__not_in' => array($post->ID),
-            'showposts'=>$sl,
-            'ignore_sticky_posts'=>1
-        );
-        $my_query = new wp_query($args);
-        if( $my_query->have_posts() ) {
-            while ($my_query->have_posts()) { $my_query->the_post();
-                echo get_template_part('sections/post-list');
-            }
-        }
-    }
-}
--- single.php
+++ single.php
@@ -1,29 +1,17 @@
 <?php
 get_header();
+get_template_part('sections/home/focus'); 
+get_template_part('sections/banner-top-on-all-page');
 ?>
 <section class="default_single_post">
-    <div class="banner_single">
-        <img src="<?php echo get_field("banner_single_page","option"); ?>" alt="banner_single">
-    </div>
-    <div class="container">
+    <div class="mc-container">
         <div class="single_contain">
             <?php 
             if(have_posts()):
                 while(have_posts()) : the_post();
             ?>
-            <div class="breadcum-contain">
-                <p><a href="<?php echo get_home_url(); ?>"><i class="fas fa-home"></i>&nbsp;Home
-                        page</a>
-                </p>
-                <p><i class="fas fa-forward"></i></p>
-                <p class="bre-title">
-                    <?php
-                        the_title();
-                  ?>
-                </p>
-            </div>
-            <div class="sg-row">
-                <div class="sg-col-left">
+            <div class="mc-row mc-mg--15">
+                <div class="mc-col-9 mc-pdx-15">
                     <div class="detail_post">
                         <div class="title_sg_post"><?php the_title(); ?></div>
                         <div class="date-content">
@@ -34,17 +22,58 @@ get_header();
                         </div>
                     </div>
                 </div>
-                <div class="sg-col-right">
-                    <div class="sidebar">
-                        <a href="#" class="banner-sidebar">
-                            <img src="<?php echo get_field('banner_right_single','option'); ?>" alt="banner-sidebar">
-                        </a>
-                        <div class="contain-sidebar">
-                            <div class="title-post-lq">
-                                Bài viết liên quan
-                            </div>
-                            <?php loop_single_related_post(4); ?>
+                <div class="mc-col-3 mc-pdx-15">
+                    <?php 
+                        $args = array( 
+                            'category_name' => 'hon-nhan-va-gia-dinh',
+                            'post_type'=>  'post',
+                            'post_status' => 'publish',
+                            'offset' => 4, 
+                            'posts_per_page' => 1,
+                        );
+                        $post_one_1 = new WP_Query( $args );
+                        if ( $post_one_1->have_posts() ) :
+                        while ( $post_one_1->have_posts() ) : $post_one_1->the_post();
+                    ?>
+                    <a href="<?php the_permalink(); ?>" class="ns-first-post-c2">
+                        <div class="ns-thumb-post-c2">
+                            <img src="<?php echo get_the_post_thumbnail_url(); ?>" alt=" <?php echo get_the_title(); ?>">
                         </div>
+                        <div class="ns-title-post-c2">
+                            <?php echo get_the_title(); ?>
+                        </div>
+                    </a>
+                    <?php 
+                        endwhile;
+                        endif;
+                        wp_reset_postdata();
+                    ?>
+                    <div class="nc-list-before-first-c2">
+                        <?php 
+                            $args = array( 
+                                'category_name' => 'hon-nhan-va-gia-dinh',
+                                'post_type'=>  'post',
+                                'post_status' => 'publish',
+                                'offset' => 5, 
+                                'posts_per_page' => 3,
+                            );
+                            $post_one_1 = new WP_Query( $args );
+                            if ( $post_one_1->have_posts() ) :
+                            while ( $post_one_1->have_posts() ) : $post_one_1->the_post();
+                        ?>
+                        <a href="<?php the_permalink(); ?>" class="mc-row mc-mg--10">
+                            <div class="ns-thumb-post-horizontal-c2 mc-col-6 mc-pdx-10">
+                                <img src="<?php echo get_the_post_thumbnail_url(); ?>" alt=" <?php echo get_the_title(); ?>">
+                            </div>
+                            <div class="ns-title-post-horizontal-c2 mc-col-6 mc-pdx-10">
+                                <?php echo wp_trim_words( get_the_title(), 14, '...' ); ?>
+                            </div>
+                        </a>
+                        <?php 
+                            endwhile;
+                            endif;
+                            wp_reset_postdata();
+                        ?>
                     </div>
                 </div>
             </div>
